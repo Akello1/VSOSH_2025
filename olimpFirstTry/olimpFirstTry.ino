d6a1c9c63fe617bae152c6d2f5ea0c71b0c2fe73
Аким Акелло <akim.petbu@gmail.com>
2025-02-14 15:52:19 +0300

    Line Sensor Done + comment

--- olimpFirstTry/olimpFirstTry.ino
+++ olimpFirstTry/olimpFirstTry.ino
@@ -1,27 +1,35 @@
-const int lineTr1 = A11;
-const int lineTr2 = A12;
-int lTrState1 = 0;
-int lTrState2 = 0;
+const int lineTr1 = A11; //Line Pin
+const int lineTr2 = A12; //Line Pin
+int lTrState1 = 0; //Line State
+int lTrState2 = 0; //Line State
 
 void setup() {
-    pinMode(lineTr1, INPUT);
-    pinMode(lineTr2, INPUT);
+    pinMode(lineTr1, INPUT); //Line
+    pinMode(lineTr2, INPUT); //Line
     Serial.begin(9600);
 }
 
 void loop() {
-    lTrState1 = digitalRead(lineTr1);
-    lTrState2 = digitalRead(lineTr2);
-    if (lTrState1 == HIGH)
+    lTrState1 = digitalRead(lineTr1); //Line
+    lTrState2 = digitalRead(lineTr2); //Line
+    if (lTrState1 == HIGH) //Line
     {
-        Sreial.println("White 1")
-    }
+        Sreial.println("White 1") //Line
+    }  //Line
     else
-        Serial.println("Black 1")
-        if (lTrState2 == HIGH)
-        {
-            Sreial.println("White 2")
-        }
-        else
-            Serial.println("Black 1")
+    {
+        Serial.println("Black 1") //Line
+    } //Line
+    if (lTrState2 == HIGH) //Line
+    {
+        Sreial.println("White 2") //Line
+    } //Line
+    else
+    {
+        Serial.println("Black 2") //Line
+    } //Line
+
+
+    delay(50);
+           
 }
