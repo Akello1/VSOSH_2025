87dbaef07b59ed166bc0075b6fb5f0d55d3874ad
Аким Акелло <akim.petbu@gmail.com>
2025-02-14 15:41:14 +0300

    Line Udate

--- olimpFirstTry/olimpFirstTry.ino
+++ olimpFirstTry/olimpFirstTry.ino
@@ -1,14 +1,27 @@
 const int lineTr1 = A11;
 const int lineTr2 = A12;
-int lTrState = 0;
-int lTrState = 0;
+int lTrState1 = 0;
+int lTrState2 = 0;
 
 void setup() {
-  pinMode(lineTr1,INPUT)
-  pinMode(lineTr2,INPUT)
-  Serial.begin(9600)
+    pinMode(lineTr1, INPUT);
+    pinMode(lineTr2, INPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
-  
+    lTrState1 = digitalRead(lineTr1);
+    lTrState2 = digitalRead(lineTr2);
+    if (lTrState1 == HIGH)
+    {
+        Sreial.println("White 1")
+    }
+    else
+        Serial.println("Black 1")
+        if (lTrState2 == HIGH)
+        {
+            Sreial.println("White 2")
+        }
+        else
+            Serial.println("Black 1")
 }
