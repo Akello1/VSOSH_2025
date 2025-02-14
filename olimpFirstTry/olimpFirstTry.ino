9224b34a98f078b76c1fc0f2bbe0384aafcda111
Аким Акелло <akim.petbu@gmail.com>
2025-02-14 16:36:21 +0300

    Start Motors

--- olimpFirstTry/olimpFirstTry.ino
+++ olimpFirstTry/olimpFirstTry.ino
@@ -1,3 +1,8 @@
+# include <Vex5.h> //Motor
+
+Vex5_Motor motor_right;//Motor R
+Vex5_Motor motor_left;//Motor L
+
 const int lineTr1 = A11; //Line Pin
 const int lineTr2 = A12; //Line Pin
 
@@ -10,6 +15,9 @@ int lTrState2 = 0; //Line State
 unsigned int DistTime = 0; //Line State
 unsigned int Distance = 0; //Line State
 
+int leftSpeed = 200; //Motor R speed
+int rightSpeed = -200; //Motor L speed
+float kp = 4.5; //Motor
 
 void setup() {
     pinMode(lineTr1, INPUT); //Line
@@ -18,7 +26,8 @@ void setup() {
     pinMode(DistSend, OUTPUT); //UZ Sensor
     pinMode(DistGet, OUTPUT); //UZ Sensor
 
-
+    Vex5.begin(); //Motor
+    motor.begin(VEX5_PORT_1) //Motor
 
     Serial.begin(9600);
 }
