ce9b6094a745903cca8f2562fd4d0462a79555bf
Akello1 <132712405+Akello1@users.noreply.github.com>
2025-02-14 15:26:52 +0300

    LineSensors
    Начал работу с датчиками расстояния

---
+++ olimpFirstTry/olimpFirstTry.ino
@@ -0,0 +1,14 @@
+const int lineTr1 = A11;
+const int lineTr2 = A12;
+int lTrState = 0;
+int lTrState = 0;
+
+void setup() {
+  pinMode(lineTr1,INPUT)
+  pinMode(lineTr2,INPUT)
+  Serial.begin(9600)
+}
+
+void loop() {
+  
+}
