11027f8fd9721140c8a223b31ae547175c7b2a4e
Аким Акелло <akim.petbu@gmail.com>
2025-02-14 16:24:53 +0300

    UZ Sensor Added

--- olimpFirstTry/olimpFirstTry.ino
+++ olimpFirstTry/olimpFirstTry.ino
@@ -1,11 +1,25 @@
 const int lineTr1 = A11; //Line Pin
 const int lineTr2 = A12; //Line Pin
+
+const int DistSend = 33; //UZ Sensor Pin
+const int DistGet = 34; //UZ Sensor Pin
+
 int lTrState1 = 0; //Line State
 int lTrState2 = 0; //Line State
+
+unsigned int DistTime = 0; //Line State
+unsigned int Distance = 0; //Line State
+
 
 void setup() {
     pinMode(lineTr1, INPUT); //Line
     pinMode(lineTr2, INPUT); //Line
+
+    pinMode(DistSend, OUTPUT); //UZ Sensor
+    pinMode(DistGet, OUTPUT); //UZ Sensor
+
+
+
     Serial.begin(9600);
 }
 
@@ -29,7 +43,13 @@ void loop() {
         Serial.println("Black 2") //Line
     } //Line
 
+    digitalWrite(DistSend, 1); //UZ Sensor Send
+    delayMicroseconds(10);//UZ Sensor Send
+    digitalWrite(DistSend, LOW);//UZ Sensor Send
+    DistTime = pulseln(DistGet, HIGH); //UZ Sensor Time
+    Distance = DistTime / 58; //UZ Sensor Len
+    Serial.println($"Distance {Distance}");
 
-    delay(50);
+    delay(100);
            
 }
